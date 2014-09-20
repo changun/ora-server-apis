a1e514fffe230c24c2fdccba03bccc876c5469e1
changun <changun.tw@gmail.com>
2014-09-20 01:03:06 -0400

    Initial commit

---
+++ urls.js
@@ -0,0 +1,4 @@
+var loginUrl = "http://lifestreams.smalldata.io/login";
+var authUrl = "http://lifestreams.smalldata.io/oauth/auth";
+var checkAuthUrl = "http://lifestreams.smalldata.io/oauth/check-auth";
+var oraBaseUrl = "http://lifestreams.smalldata.io/ora/";
